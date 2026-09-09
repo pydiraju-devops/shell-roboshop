26e516f15bc3536fc901d1ddf2852206966b5618
pydiraju <pydiraju.tech@gmail.com>
2026-09-09 12:53:46 +0530

    aws instance launch

--- aws.sh
+++ aws.sh
@@ -13,22 +13,22 @@ do
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]'\
-    --query 'Instances[0].instanceid' \
+    --query 'Instances[0].InstanceId' \
     --output text )
 
-    if [ $instance == frontend ]; then
+    if [ $instance == "frontend" ]; then
         IP=$(
               aws ec2 describe-instances \
               --instance-ids $INSTANCE_ID \
-              --query 'Reservations[].Instances[].PublicIpAddress}' \
+              --query 'Reservations[].Instances[].PublicIpAddress' \
               --output text
 
-    )
+        )
     else 
         IP=$(
               aws ec2 describe-instances \
               --instance-ids $INSTANCE_ID \
-              --query 'Reservations[].Instances[].privateIpAddress}' \
+              --query 'Reservations[].Instances[].PrivateIpAddress' \
               --output text
     )
     fi
