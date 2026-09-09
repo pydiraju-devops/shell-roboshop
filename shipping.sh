aa6e10ad3c8537f746cf4f3e6165c9a1ea884506
pydiraju <pydiraju.tech@gmail.com>
2026-09-10 03:55:31 +0530

    aws instance launch

--- shipping.sh
+++ shipping.sh
@@ -68,9 +68,17 @@ VALIDATE $? "Created systemctl service"
 dnf install mysql -y  &>> $LOGS_FILE
 VALIDATE $? "installing mysql client"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>> $LOGS_FILE
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>> $LOGS_FILE
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>> $LOGS_FILE
+mysql -h $MYSQL_HOST -uroot -pRoboshop@1 -e "use cities"
+
+if [ $? -ne 0 ]; then
+   
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>> $LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>> $LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>> $LOGS_FILE
+    VALIDATE $? "data loading into mysql"
+else    
+    echo -e "data loaded ..$Y skipping $N"
+fi
 
 systemctl enable shipping &>> $LOGS_FILE
 VALIDATE $? "enable"
