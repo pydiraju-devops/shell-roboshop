79e33b3c745c95caabbe0912887943fe73491584
pydiraju <pydiraju.tech@gmail.com>
2026-09-09 19:45:15 +0530

    aws instance launch

--- roboshop.sh
+++ roboshop.sh
@@ -1,66 +1,65 @@
 #!/bin/bash
-set -e
-trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
-
 
 SG_ID="sg-078da222b6b5e6e14"
 AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z05434983MKHE7P63RH25"
 DOMAIN_NAME="pydiraju.online"
 
-for instance in $@
-do 
-    INSTANCE_ID=$( aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type "t3.micro" \
-    --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text )
+for instance in "$@"
+do
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type "t3.micro" \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
 
-    if [ $instance == "frontend" ]; then
-        IP=$(
-              aws ec2 describe-instances \
-              --instance-ids $INSTANCE_ID \
-              --query 'Reservations[].Instances[].PublicIpAddress' \
-              --output text
+    if [ "$instance" == "frontend" ]; then
 
-        )
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text)
+
         RECORD_NAME="$DOMAIN_NAME"
-    else 
-        IP=$(
-              aws ec2 describe-instances \
-              --instance-ids $INSTANCE_ID \
-              --query 'Reservations[].Instances[].PrivateIpAddress' \
-              --output text
-    )
+
+    else
+
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text)
+
         RECORD_NAME="$instance.$DOMAIN_NAME"
 
     fi
-    echo "ip address $IP"
+
+    echo "IP Address: $IP"
 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Updating an record",
-        "Changes": [
-            {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": "'$RECORD_NAME'",
-                "Type": "A",
-                "TTL": 1,
-                "ResourceRecords": [
+        --hosted-zone-id "$ZONE_ID" \
+        --change-batch '
+        {
+            "Comment": "Updating record",
+            "Changes": [
                 {
-                    "Value": "'$IP'"
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "'$RECORD_NAME'",
+                        "Type": "A",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                            {
+                                "Value": "'$IP'"
+                            }
+                        ]
+                    }
                 }
-                ]
-            }
-            }
-        ]
+            ]
         }
         '
-    echo "record updated $instance"
-done
 
+    echo "record updated for $instance"
+
+done
