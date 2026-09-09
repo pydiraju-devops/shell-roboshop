77e3ef7a6d6f720346fb14ae9a41ba90487275d5
pydiraju <pydiraju.tech@gmail.com>
2026-09-10 00:56:36 +0530

    aws instance launch

--- catalogue.sh
+++ catalogue.sh
@@ -1,86 +1,76 @@
 #!/bin/bash
 
-
-USER_ID=$(id -u)
-
+USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.pydiraju.online
 
-if [ $USER_ID -ne 0 ]; then
-    echo -e "$R Run this script with root user $N" | tee -a "$LOGS_FILE"
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
-mkdir -p "$LOGS_FOLDER"
+mkdir -p $LOGS_FOLDER
 
-VALIDATE() {
-
+VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2 is $R failure $N" | tee -a "$LOGS_FILE"
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$2 is $G success $N" | tee -a "$LOGS_FILE"
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
-dnf module disable nodejs -y &>> $LOGS_FILE
-VALIDATE $? "disable nodejs"
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling NodeJS Default version"
 
-dnf module enable nodejs:20 -y &>> $LOGS_FILE
-VALIDATE $? "enable nodejs 20"
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20"
 
-dnf install nodejs -y &>> $$LOGS_FILE
-VALIDATE $? "installing nodejs"
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Install NodeJS"
 
-id roboshop &>> $LOGS_FILE
+id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
-    VALIDATE $? "user added"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
 else
-    echo -e "user already exist ...$Y skipping $N"    
-
-fi    
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
 
 mkdir -p /app 
-VALIDATE $? "created directory"
+VALIDATE $? "Creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $LOGS_FILE
-VALIDATE $? "unzipping code"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOGS_FILE
+VALIDATE $? "Downloading catalogue code"
 
-cd /app 
-VALIDATE $? "path change to /app"
+cd /app
+VALIDATE $? "Moving to app directory"
 
 rm -rf /app/*
-VALIDATE $? "removing existing code"
+VALIDATE $? "Removing existing code"
 
-unzip /tmp/catalogue.zip &>> $LOGS_FILE
-VALIDATE $? "unzipping code"
+unzip /tmp/catalogue.zip &>>$LOGS_FILE
+VALIDATE $? "Uzip catalogue code"
 
-npm install  &>> $LOGS_FILE
-VALIDATE $? "npm installation"
+npm install  &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
 
-cp $SCRIPT_DIR/catalogue.service  /etc/systemd/system/catalogue.service &>> $LOGS_FILE
-VALIDATE $? "service created"
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Created systemctl service"
 
-systemctl daemon-reload &>> $LOGS_FILE
-VALIDATE $? "reload"
-
-systemctl enable catalogue &>> $LOGS_FILE
-systemctl start catalogue &>> $LOGS_FILE
-VALIDATE $? "enabling and start"
+systemctl daemon-reload
+systemctl enable catalogue  &>>$LOGS_FILE
+systemctl start catalogue
+VALIDATE $? "Starting and enabling catalogue"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "copied"
-
-dnf install mongodb-mongosh -y
-VALIDATE $? "installing client db"
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
 
 INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
@@ -92,4 +82,4 @@ else
 fi
 
 systemctl restart catalogue
-VALIDATE $? "restarting service"
+VALIDATE $? "Restarting catalogue"
