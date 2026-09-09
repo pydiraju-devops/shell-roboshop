6ae8e815356d2f811c9c18f453c0748422e5af27
pydiraju <pydiraju.tech@gmail.com>
2026-09-09 12:35:53 +0530

    aws instance launch

---
+++ aws.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SG_ID="sg-078da222b6b5e6e14"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do 
+    instance_id=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]'\
+    --query 'Instances[0].instanceid' \
+    --output text )
+
+    if [ $instance == frontend ]; then
+        IP=$(
+              aws ec2 describe-instances \
+              --instance-ids $instance_id \
+              --query 'Reservations[].Instances[].PublicIpAddress}' \
+              --output text
+
+    )
+    else 
+        IP=$(
+              aws ec2 describe-instances \
+              --instance-ids $instance_id \
+              --query 'Reservations[].Instances[].privateIpAddress}' \
+              --output text
+    )
+    fi
+done
+
