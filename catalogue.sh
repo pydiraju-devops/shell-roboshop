7d94874254814737502e60066ed1abaaca2d59df
pydiraju <pydiraju.tech@gmail.com>
2026-09-10 00:32:32 +0530

    aws instance launch

--- catalogue.sh
+++ catalogue.sh
@@ -5,12 +5,13 @@ USER_ID=$(id -u)
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-SCRIPT_DIR=$PWD
+
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SCRIPT_DIR=$PWD
 
 if [ $USER_ID -ne 0 ]; then
     echo -e "$R Run this script with root user $N" | tee -a "$LOGS_FILE"
